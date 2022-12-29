723c60d47d0db78bae6f29b7be14eb4a7b3fc804
Project-S-31415 <projects31415.gif@gmail.com>
2022-12-29 13:27:15 +0900

    プロジェクト ファイルを追加します。

---
+++ MusicEditerMini/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicEditerMini
+{
+    public partial class MEM : Form
+    {
+        public MEM()
+        {
+            InitializeComponent();
+        }
+
+        private void MEM_Load(object sender, EventArgs e)
+        {
+           if(!File.Exists("ffmpeg.exe"))
+            {
+
+
+            }
+        }
+    }
+}
